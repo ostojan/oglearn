3b6ba64e63a7621516f822d2a1f9d2ab06c3f895
ostojan <a.stojanowski@outlook.com>
2022-05-07 22:25:19 +0200

    File with resource paths

---
+++ app/inc/utils/Paths.hpp
@@ -0,0 +1,13 @@
+#ifndef APP_INC_UTILS_PATHS_HPP
+#define APP_INC_UTILS_PATHS_HPP
+
+namespace oglearn::utils::paths
+{
+    namespace shaders
+    {
+        constexpr const char *basicVertexShader = "shaders/basic/vertex.glsl";
+        constexpr const char *basicFragmentShader = "shaders/basic/fragment.glsl";
+    }
+}
+
+#endif
