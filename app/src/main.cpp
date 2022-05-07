c6c299d6c87dae60910b7fb1257bf698e51cdd9a
ostojan <a.stojanowski@outlook.com>
2022-05-07 22:41:32 +0200

    Create buffers for first triangle

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 #include <stdexcept>
 
@@ -73,6 +74,28 @@ int main(int argCount, const char *const *const argValues)
             program.AttachShader(vertexShader);
             program.AttachShader(fragmentShader);
             program.Link();
+        }
+
+        GLuint vao = -1;
+        {
+            glGenVertexArrays(1, &vao);
+            glBindVertexArray(vao);
+
+            std::array vertexAttributes = {
+                -0.5f, -0.5f, 0.0f, // 1st
+                0.5f, -0.5f, 0.0f,  // 2nd
+                0.0f, 0.5f, 0.0f,   // 3rd
+            };
+
+            GLuint vbo = -1;
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertexAttributes), vertexAttributes.data(), GL_STATIC_DRAW);
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+            glEnableVertexAttribArray(0);
+
+            glBindVertexArray(0);
         }
 
         while (!glfwWindowShouldClose(window))
